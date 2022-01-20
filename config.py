0054a86d52196bee504b793b422291b561110ca5
ryohidaka <git@ryohidaka.jp>
2022-01-20 23:26:14 +0900

    feat: 定期実行用コードを追加

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # .env ファイルをロードして環境変数へ反映
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
 # 環境変数を参照
-import os
-CONSUMER_KEY = os.getenv('CONSUMER_KEY')
-CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
-ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
-ACCESS_SECRET = os.getenv('ACCESS_SECRET')
+CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+ACCESS_SECRET = os.environ.get('ACCESS_SECRET')
